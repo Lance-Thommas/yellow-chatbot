0b02138533d44876c2ae4d431217929427e51f28
Lance-Thommas <lance.gicano@gmail.com>
2025-09-30 00:22:51 +0400

    Reverted App.jsx changes (AppWrapper Removed)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,52 +3,37 @@ import {
   Routes,
   Route,
   Navigate,
-  useNavigate,
 } from "react-router-dom";
 import Login from "./pages/Login";
 import Projects from "./pages/Projects";
 import ProjectDetail from "./pages/ProjectDetail";
 import ProtectedRoute from "./components/ProtectedRoute";
 
-function AppWrapper() {
-  const navigate = useNavigate();
-
-  const handleLogin = () => {
-    navigate("/projects");
-  };
-
-  return <App onLogin={handleLogin} />;
-}
-
-function App({ onLogin }) {
-  return (
-    <Routes>
-      <Route path="/login" element={<Login onLogin={onLogin} />} />
-      <Route
-        path="/projects"
-        element={
-          <ProtectedRoute>
-            <Projects />
-          </ProtectedRoute>
-        }
-      />
-      <Route
-        path="/projects/:projectId"
-        element={
-          <ProtectedRoute>
-            <ProjectDetail />
-          </ProtectedRoute>
-        }
-      />
-      <Route path="*" element={<Navigate to="/projects" replace />} />
-    </Routes>
-  );
-}
-
-export default function AppRouter() {
+function App() {
   return (
     <Router>
-      <AppWrapper />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/projects"
+          element={
+            <ProtectedRoute>
+              <Projects />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/projects/:projectId"
+          element={
+            <ProtectedRoute>
+              <ProjectDetail />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="*" element={<Navigate to="/projects" replace />} />
+      </Routes>
     </Router>
   );
 }
+
+export default App;
