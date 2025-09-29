c6ded0e6fa1d0213dbbb92a319c9dd66011c8ee0
Lance-Thommas <lance.gicano@gmail.com>
2025-09-29 12:47:17 +0400

    Updated client.js

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Create a reusable axios instance
 const api = axios.create({
-  baseURL: "https://yellow-chatbot.onrender.com", // Render backend URL
+  baseURL: "https://yellow-chatbot.onrender.com/api", // Render backend URL
   withCredentials: true, // include cookies if needed
 });
 
